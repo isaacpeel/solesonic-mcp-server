055463fecdb06a655dae282d388186d0813348d8
isaac <isaac@gmail.com>
2025-08-13 05:48:52 -0600

    Initial commit

---
+++ src/main/java/com/solesonic/mcp/SolesonicMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.solesonic.mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolesonicMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolesonicMcpServerApplication.class, args);
+    }
+
+}
