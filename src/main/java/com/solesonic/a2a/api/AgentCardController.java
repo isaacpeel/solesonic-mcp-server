661d67337eef4b31a0c569bb47a1e8b657171e24
Isaac Peel <isaacpeel@gmail.com>
2026-05-17 00:57:23 -0600

    I'm updated my mistake, you were right I was wrong

--- src/main/java/com/solesonic/a2a/api/AgentCardController.java
+++ src/main/java/com/solesonic/a2a/api/AgentCardController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/a2a")
-@PreAuthorize("hasAuthority('ROLE_AGENT-EXECUTE')")
 public class AgentCardController {
     private static final Logger log = LoggerFactory.getLogger(AgentCardController.class);
 
